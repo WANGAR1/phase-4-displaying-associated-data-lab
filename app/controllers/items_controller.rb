fea852a910913909f9729c48c7c8c4d0b3d1a922
Wangari Samantha <samanthanyams5@gmail.com>
2023-07-03 06:52:16 +0300

    complete

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,2 +1,7 @@
 class ItemsController < ApplicationController
+    def index
+      items = Item.all
+      render json: items, include: { user: { only: [:id, :username, :city] } }
+    end
 end
+  
